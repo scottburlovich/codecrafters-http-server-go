654b6da2759f58a4d217420453b8e6f06a543d08
Scott Burlovich <teedot@gmail.com>
2024-04-13 01:39:23 -0400

    stage 2

--- app/server.go
+++ app/server.go
@@ -13,17 +13,34 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	// read data from the http connection
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Received data: %v\n", string(buf))
+
+	// respond with 'HTTP/1.1 200 OK' followed by two line breaks
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing: ", err.Error())
+		os.Exit(1)
+	}
+
 }
